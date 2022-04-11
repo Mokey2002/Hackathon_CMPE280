0790db287b6ad70c9fe1ce7ebec779638bf99611
Eduardo Lemus <edventura91@gmail.com>
2022-04-10 21:09:54 -0700

    updated graphs

--- backend/index.js
+++ backend/index.js
@@ -41,7 +41,432 @@ const { count, Console } = require('console');
 const readFile = require('fs').readFile;
 
  
+var GDP= "";
+//var finalData=[];
+var output=[];
+var myitem = [];
+readFile('\GDP USD - API_NY.GDP.MKTP.CD_DS2_en_csv_v2_3263806.csv', 'utf-8', (err, fileContent) => {
+    if(err) {
+        console.log(err); // Do something to handle the error or just throw it
+        throw new Error(err);
+    }
+      GDP = csvjson.toObject(fileContent);
 
+      output = GDP.map(Object.values);//array of objects is turned to array of arrays
+      var samplearray=[];
+    for (var i=0;i<output.length;i++){
+    for(var j =0;j<output[i].length;j++){
+      samplearray.push([parseFloat(output[i][j])]);//turn values to float
+    }}
+    //merge arrays within array to match format
+    for(var i =0;i<samplearray.length;i=i+4){
+      myitem.push([...samplearray[i],...samplearray[i+1],...samplearray[i+2],...samplearray[i+3]]);
+    }
+      myitem.splice(0,0,['Year','INDIA','CHINA','USA']);//add column headers at beggining of array   
+});
+var GDP1= "";
+//var finalData=[];
+var output1=[];
+var myitem1 = [];
+readFile('\GDP Growth Rate -API_NY.GDP.MKTP.KD.ZG_DS2_en_csv_v2_3158928.csv', 'utf-8', (err, fileContent) => {
+    if(err) {
+        console.log(err); // Do something to handle the error or just throw it
+        throw new Error(err);
+    }
+      GDP1 = csvjson.toObject(fileContent);
+      output1 = GDP1.map(Object.values);//array of objects is turned to array of arrays
+      var samplearray=[];
+    for (var i=0;i<output1.length;i++){
+    for(var j =0;j<output1[i].length;j++){
+      samplearray.push([parseFloat(output1[i][j])]);//turn values to float
+    }}
+    //merge arrays within array to match format
+    for(var i =0;i<samplearray.length;i=i+4){
+      myitem1.push([...samplearray[i],...samplearray[i+1],...samplearray[i+2],...samplearray[i+3]]);
+    }
+      myitem1.splice(0,0,['Year','INDIA','CHINA','USA']);//add column headers at beggining of array   
+});
+
+var GDP2= "";
+//var finalData=[];
+var output2=[];
+var myitem2 = [];
+readFile('\CurrentAccountBalance-API_BN.CAB.XOKA.GD.ZS_DS2_en_csv_v2_3158921 (1).csv', 'utf-8', (err, fileContent) => {
+    if(err) {
+        console.log(err); // Do something to handle the error or just throw it
+        throw new Error(err);
+    }
+      GDP2 = csvjson.toObject(fileContent);
+      output2 = GDP2.map(Object.values);//array of objects is turned to array of arrays
+      var samplearray=[];
+    for (var i=0;i<output2.length;i++){
+    for(var j =0;j<output2[i].length;j++){
+      samplearray.push([parseFloat(output2[i][j])]);//turn values to float
+    }}
+    //merge arrays within array to match format
+    for(var i =0;i<samplearray.length;i=i+4){
+      myitem2.push([...samplearray[i],...samplearray[i+1],...samplearray[i+2],...samplearray[i+3]]);
+    }
+      myitem2.splice(0,0,['Year','CHINA','INDIA','USA']);//add column headers at beggining of array   
+});
+
+var GDP3= "";
+//var finalData=[];
+var output3=[];
+var myitem3 = [];
+readFile('\Foreign direct investment, net (BoP, current US$) - API_BN.KLT.DINV.CD_DS2_en_csv_v2_3158846.csv', 'utf-8', (err, fileContent) => {
+    if(err) {
+        console.log(err); // Do something to handle the error or just throw it
+        throw new Error(err);
+    }
+      GDP3 = csvjson.toObject(fileContent);
+      output3 = GDP3.map(Object.values);//array of objects is turned to array of arrays
+      var samplearray=[];
+    for (var i=0;i<output3.length;i++){
+    for(var j =0;j<output3[i].length;j++){
+      samplearray.push([parseFloat(output3[i][j])]);//turn values to float
+    }}
+    //merge arrays within array to match format
+    for(var i =0;i<samplearray.length;i=i+4){
+      myitem3.push([...samplearray[i],...samplearray[i+1],...samplearray[i+2],...samplearray[i+3]]);
+    }
+      myitem3.splice(0,0,['Year','CHINA','INDIA','USA']);//add column headers at beggining of array   
+});
+
+var GDP4= "";
+//var finalData=[];
+var output4=[];
+var myitem4 = [];
+readFile('\Foreign direct investment, net outflows (BoP, current US$) - BM.KLT.DINV.CD.WD (1).csv', 'utf-8', (err, fileContent) => {
+    if(err) {
+        console.log(err); // Do something to handle the error or just throw it
+        throw new Error(err);
+    }
+      GDP4 = csvjson.toObject(fileContent);
+      output4 = GDP4.map(Object.values);//array of objects is turned to array of arrays
+      var samplearray=[];
+    for (var i=0;i<output4.length;i++){
+    for(var j =0;j<output4[i].length;j++){
+      samplearray.push([parseFloat(output4[i][j])]);//turn values to float
+    }}
+    //merge arrays within array to match format
+    for(var i =0;i<samplearray.length;i=i+4){
+      myitem4.push([...samplearray[i],...samplearray[i+1],...samplearray[i+2],...samplearray[i+3]]);
+    }
+      myitem4.splice(0,0,['Year','CHINA','INDIA','USA']);//add column headers at beggining of array   
+});
+
+var GDP5= "";
+//var finalData=[];
+var output5=[];
+var myitem5 = [];
+readFile('\Foreign direct investment, net inflows (% of GDP) - API_BX.KLT.DINV.WD.GD.ZS_DS2_en_csv_v2_3159100 (1).csv', 'utf-8', (err, fileContent) => {
+    if(err) {
+        console.log(err); // Do something to handle the error or just throw it
+        throw new Error(err);
+    }
+      GDP5 = csvjson.toObject(fileContent);
+      output5 = GDP5.map(Object.values);//array of objects is turned to array of arrays
+      var samplearray=[];
+    for (var i=0;i<output5.length;i++){
+    for(var j =0;j<output5[i].length;j++){
+      samplearray.push([parseFloat(output5[i][j])]);//turn values to float
+    }}
+    //merge arrays within array to match format
+    for(var i =0;i<samplearray.length;i=i+4){
+      myitem5.push([...samplearray[i],...samplearray[i+1],...samplearray[i+2],...samplearray[i+3]]);
+    }
+      myitem5.splice(0,0,['Year','CHINA','INDIA','USA']);//add column headers at beggining of array   
+});
+
+var GDP6= "";
+//var finalData=[];
+var output6=[];
+var myitem6 = [];
+readFile('\FDI-NetOutflows(%ofGDP)- API_BM.KLT.DINV.WD.GD.ZS_DS2_en_csv_v2_3158853 (1).csv', 'utf-8', (err, fileContent) => {
+    if(err) {
+        console.log(err); // Do something to handle the error or just throw it
+        throw new Error(err);
+    }
+      GDP6 = csvjson.toObject(fileContent);
+      output6 = GDP6.map(Object.values);//array of objects is turned to array of arrays
+      var samplearray=[];
+    for (var i=0;i<output6.length;i++){
+    for(var j =0;j<output6[i].length;j++){
+      samplearray.push([parseFloat(output6[i][j])]);//turn values to float
+    }}
+    //merge arrays within array to match format
+    for(var i =0;i<samplearray.length;i=i+4){
+      myitem6.push([...samplearray[i],...samplearray[i+1],...samplearray[i+2],...samplearray[i+3]]);
+    }
+      myitem6.splice(0,0,['Year','CHINA','INDIA','USA']);//add column headers at beggining of array   
+});
+
+app.post("/getGDP", (req,res)=>{
+  let data = req.body.selectedOption;
+  var abc = [];
+  if(data=='INDIA'){
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem, 3);
+    abc = removeEl(temp, 2);
+    res.send(JSON.stringify(abc));
+  }
+  if(data=='CHINA'){
+
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem, 1);
+    abc = removeEl(temp, 2);   
+    res.send(JSON.stringify(abc));
+  }
+  if(data=='USA'){
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem, 1);
+    abc = removeEl(temp, 1);    
+    res.send(JSON.stringify(abc));
+  }
+  console.log(abc); 
+});
+
+app.post("/getGDP1", (req,res)=>{
+  let data = req.body.selectedOption;
+  var abc = [];
+  if(data=='INDIA'){
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem1, 3);
+    abc = removeEl(temp, 2);
+    res.send(JSON.stringify(abc));
+  }
+  if(data=='CHINA'){
+
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem1, 1);
+    abc = removeEl(temp, 2);   
+    res.send(JSON.stringify(abc));
+  }
+  if(data=='USA'){
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem1, 1);
+    abc = removeEl(temp, 1);    
+    res.send(JSON.stringify(abc));
+  }
+  console.log(abc); 
+});
+
+app.post("/getGDP2", (req,res)=>{
+  let data = req.body.selectedOption;
+  var abc = [];
+  if(data=='INDIA'){
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem2, 1);
+    abc = removeEl(temp, 2);
+    res.send(JSON.stringify(abc));
+  }
+  if(data=='CHINA'){
+
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem2, 3);
+    abc = removeEl(temp, 2);   
+    res.send(JSON.stringify(abc));
+  }
+  if(data=='USA'){
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem2, 1);
+    abc = removeEl(temp, 1);    
+    res.send(JSON.stringify(abc));
+  }
+  console.log(abc); 
+});
+
+
+app.post("/getGDP3", (req,res)=>{
+  let data = req.body.selectedOption;
+  var abc = [];
+  if(data=='INDIA'){
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem3, 1);
+    abc = removeEl(temp, 2);
+    res.send(JSON.stringify(abc));
+  }
+  if(data=='CHINA'){
+
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem3, 3);
+    abc = removeEl(temp, 2);   
+    res.send(JSON.stringify(abc));
+  }
+  if(data=='USA'){
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem3, 1);
+    abc = removeEl(temp, 1);    
+    res.send(JSON.stringify(abc));
+  }
+  console.log(abc); 
+});
+
+
+
+app.post("/getGDP4", (req,res)=>{
+  let data = req.body.selectedOption;
+  var abc = [];
+  if(data=='INDIA'){
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem4, 1);
+    abc = removeEl(temp, 2);
+    res.send(JSON.stringify(abc));
+  }
+  if(data=='CHINA'){
+
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem4, 3);
+    abc = removeEl(temp, 2);   
+    res.send(JSON.stringify(abc));
+  }
+  if(data=='USA'){
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem4, 1);
+    abc = removeEl(temp, 1);    
+    res.send(JSON.stringify(abc));
+  }
+  console.log(abc); 
+});
+
+
+
+app.post("/getGDP5", (req,res)=>{
+  let data = req.body.selectedOption;
+  var abc = [];
+  if(data=='INDIA'){
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem5, 1);
+    abc = removeEl(temp, 2);
+    res.send(JSON.stringify(abc));
+  }
+  if(data=='CHINA'){
+
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem5, 3);
+    abc = removeEl(temp, 2);   
+    res.send(JSON.stringify(abc));
+  }
+  if(data=='USA'){
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem5, 1);
+    abc = removeEl(temp, 1);    
+    res.send(JSON.stringify(abc));
+  }
+  console.log(abc); 
+});
+
+
+
+app.post("/getGDP6", (req,res)=>{
+  let data = req.body.selectedOption;
+  var abc = [];
+  if(data=='INDIA'){
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem6, 1);
+    abc = removeEl(temp, 2);
+    res.send(JSON.stringify(abc));
+  }
+  if(data=='CHINA'){
+
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem6, 3);
+    abc = removeEl(temp, 2);   
+    res.send(JSON.stringify(abc));
+  }
+  if(data=='USA'){
+    function removeEl(array, remIdx){
+      return array.map(function(arr){
+        return arr.filter(function(el,idx){return idx!== remIdx});
+      });
+    };
+    var temp = removeEl(myitem6, 1);
+    abc = removeEl(temp, 1);    
+    res.send(JSON.stringify(abc));
+  }
+  console.log(abc); 
+});
 
 //get Agriculture production
 app.post("/getAgriculture", (req,res)=>{
